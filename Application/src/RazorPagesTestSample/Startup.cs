6b6166385a8fbe850ca31cbbaf81a0c84f70b124
Wing Chau <wing@devtography.com>
2023-06-15 15:10:27 +0100

    comment

--- Application/src/RazorPagesTestSample/Startup.cs
+++ Application/src/RazorPagesTestSample/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RazorPagesTestSample.Data;
 
+// startup.cs
 namespace RazorPagesTestSample
 {
     public class Startup
