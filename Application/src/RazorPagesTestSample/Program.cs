4ac164f979bd26a938187d3fbb86c83f48ff5a5e
Wing Chau <wing@devtography.com>
2023-06-15 14:39:03 +0100

    comment

--- Application/src/RazorPagesTestSample/Program.cs
+++ Application/src/RazorPagesTestSample/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
 
+// some changes
 namespace RazorPagesTestSample
 {
     public class Program
